006de386f57ad19d26456ad0ff481b2e916f8d50
Nishant Desai <nishantdesai1306@gmail.com>
2019-02-17 15:54:22 +0530

    changed square api constants for productione environment

--- lib/constants/square.dart
+++ lib/constants/square.dart
@@ -1,3 +1,3 @@
-const String SQUARE_APP_ID = 'sandbox-sq0idp-sk4Atv-paij5JM87lYbzFw';
-const String SQUARE_LOCATION_ID = 'CBASEPZccWsB1v7n6YzO_qlO1iIgAQ';
-const String SQUARE_ENV = 'test';
+const String SQUARE_APP_ID = 'sq0idp-sk4Atv-paij5JM87lYbzFw';
+const String SQUARE_LOCATION_ID = 'QACMJGSQW5XJA';
+const String SQUARE_ENV = 'production';
